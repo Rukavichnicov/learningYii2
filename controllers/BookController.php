92afcf6ba4918288c3ffc7c901abdaf9c0b2af5c
Sergey Rukavichnikov <serega1998love@yandex.ru>
2023-02-15 21:35:44 +0500

    Studying the bases of creating files

--- controllers/BookController.php
+++ controllers/BookController.php
@@ -15,6 +15,12 @@ class BookController extends \yii\web\Controller
 //        \Yii::$app->db->createCommand('CREATE FULLTEXT INDEX index_description ON book (description);')->queryOne();
 //        \Yii::$app->db->createCommand('DROP INDEX index_description ON book;')->queryOne();
         $show = \Yii::$app->db->createCommand('SHOW INDEX FROM book;')->queryAll();
+//        $array = ['book' => [1, 2, 3, 4]];
+//        $json = json_encode($array);
+//        $pathApp = \Yii::getAlias('@app');
+//        $pathRuntime = \Yii::getAlias('@runtime');
+//        $pathWebroot = \Yii::getAlias('@webroot');
+//        file_put_contents($pathRuntime . '/file2.json', $json);
         return $this->render('index', ['books' => $books,]);
     }
 }
